2128ed15a6b1be1b3a6006149d19a2c479425676
ania221B <abuwaj@gmail.com>
2022-07-19 21:27:33 +0200

    buttons and styling fix + link added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,26 @@
 const card = document.querySelector('.card')
-const template = document.querySelector('template')
-const ratingList = card.querySelector('.rating-scale')
-let rates = [...ratingList.children]
 const submitBtn = card.querySelector('button[type="submit"]')
-
-ratingList.addEventListener('click', e => {
-  if (!e.target.closest('.btn[data-type="secondary"]')) return
-
-  rates.forEach(rate => {
-    rate.classList.remove('is-selected')
-  })
-
-  const selectedRate = e.target
-  selectedRate.classList.add('is-selected')
-})
+const ratingList = card.querySelector('.rating-scale')
+const rates = Array.from(ratingList.querySelectorAll('input[type="radio"]'))
+const template = document.querySelector('template')
 
 submitBtn.addEventListener('click', e => {
   e.preventDefault()
 
-  rates = [...ratingList.children]
-  const userRateIndex = rates.findIndex(rate => rate.classList.contains('is-selected'))
-  console.log(userRateIndex)
-  if (userRateIndex === -1) {
+  const userRateInput = rates.find(rate => rate.checked)
+
+  if (userRateInput === undefined) {
     const error = card.querySelector('.error')
     error.textContent = 'Please select your rating first. Thank you!'
-  } else {
-    const templateClone = template.content.cloneNode(true)
-    const ratingInfo = templateClone.querySelector('.rating-info')
-    ratingInfo.textContent = `You selected ${userRateIndex + 1} out of 5`
-
-    const cardCurrentContent = card.firstElementChild
-
-    card.replaceChild(templateClone, cardCurrentContent)
   }
+
+  const userRate = userRateInput.value
+
+  const templateClone = template.content.cloneNode(true)
+  const ratingInfo = templateClone.querySelector('.rating-info')
+  ratingInfo.textContent = `You selected ${userRate} out of 5`
+
+  const cardCurrentContent = card.firstElementChild
+
+  card.replaceChild(templateClone, cardCurrentContent)
 })
